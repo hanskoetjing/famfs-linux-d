efe0348b211a306696f946f1af21ac2bac6f44fd
hanskoetjing <hanskucing@gmail.com>
2025-07-25 22:16:04 +0100

    send owner pid

--- drivers/cxlshm/cxlshm_mm.c
+++ drivers/cxlshm/cxlshm_mm.c
@@ -16,6 +16,7 @@
 #include <linux/cxlshm_msg.h>
 #include <asm-generic/cacheflush.h>
 #include <linux/rcupdate.h>
+#include <linux/sprintf.h>
 
 
 #define DEVICE_NAME             "cxl_mmap"
@@ -32,7 +33,7 @@ struct cxl_dev_path_struct {
 	char path[FILE_PATH_LENGTH];
 };
 
-struct ownership {
+struct ownership { //TODO: add version to the struct...
 	pid_t owner_pid;
 	char ip_4_addr[17];
 	pfn_t start;
@@ -51,8 +52,9 @@ static struct ownership o;
 
 static int mmap_helper(struct file *filp, struct vm_area_struct *vma);
 static long cxl_range_helper_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
-static int get_cxl_device(void);
-static int is_owner(pid_t pid);
+int get_cxl_device(void);
+int is_owner(pid_t pid);
+pid_t get_owner_on_mem(void)
 
 
 static const struct file_operations fops = {
@@ -83,7 +85,9 @@ static vm_fault_t cxl_helper_filemap_fault(struct vm_fault *vmf)
 
 	if (!owned) {
 		pr_info("Not owned. Try to send message\n");
-		send_one_message(o.ip_4_addr, 57580, "SBGN");
+		char pid_to_send[16] = {0};
+		snprintf(pid_to_send, 15, "%d", get_owner_on_mem());
+		send_one_message(o.ip_4_addr, 57580, pid_to_send);
 		o.owner_pid = task->pid;
 		strscpy(o.ip_4_addr, "127.0.0.1", sizeof(o.ip_4_addr));
 	}
@@ -154,7 +158,7 @@ out_path_put:
 	return err;
 }
 
-static int is_owner(pid_t pid) {
+pid_t get_owner_on_mem(void) {
 	int ret = 0;
 	if (!cxl_dax_device) return -ENXIO;
 	if (!dax_alive(cxl_dax_device))
@@ -163,16 +167,22 @@ static int is_owner(pid_t pid) {
 	if (ret < 0) return ret;
 	volatile struct ownership *owner_on_mem = (volatile struct ownership *)alloc_table_start;
 	if (owner_on_mem && owner_on_mem->owner_pid > 0) {
-		pr_info("Owner on mem: %d on host: %s\n", owner_on_mem->owner_pid, owner_on_mem->ip_4_addr);
-		if (owner_on_mem->owner_pid == pid) ret = 1;
-		else ret = 0;
+		return owner_on_mem->owner_pid;
 	} else {
-		ret = 0;
+		return -ENXIO;
 	}
+
+}
+
+int is_owner(pid_t pid) {
+	int ret = 0;
+	pid_t owner_on_memory = get_owner_on_mem();
+	if (owner_on_memory > 0 && owner_on_memory == pid) ret = 1;
+	else if (owner_on_memory < 0) ret = (int) owner_on_memory;
 	return ret;
 }
 
-static int get_cxl_device(void) {
+int get_cxl_device(void) {
 	int l = lookup_daxdev(device_path, &dax_dev_num);
 	if (!l) {
 		pr_info("dax dev num: %d\n", dax_dev_num);
@@ -214,9 +224,7 @@ static long cxl_range_helper_ioctl(struct file *file, unsigned int cmd, unsigned
 			get_cxl_device();
 			break;
 		case IOCTL_FLUSH_CACHE: //as ioctl (temporary manual invoke)
-			//volatile struct vm_area_struct
 			flush_cache_range(this_vma, this_vma->vm_start, this_vma->vm_end);
-			//flush_tlb_range(this_vma, this_vma->vm_start, this_vma->vm_end);
 			zap_vma_ptes(this_vma, this_vma->vm_start, this_vma->vm_end - this_vma->vm_start); //temporary
 			pr_info("Flush CPU cache. Size: %ld\n", this_vma->vm_end - this_vma->vm_start);
 			break;
