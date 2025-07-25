026f0f8c141c15d53b7a03ae1ebd10c69ca6f6ac
hanskoetjing <hanskucing@gmail.com>
2025-07-25 21:10:43 +0100

    change var for definition

--- drivers/cxlshm/cxlshm_mm.c
+++ drivers/cxlshm/cxlshm_mm.c
@@ -168,7 +168,7 @@ static int is_owner(pid_t pid) {
 	if (!cxl_dax_device) return -ENXIO;
 	if (!dax_alive(cxl_dax_device))
 		run_dax(cxl_dax_device);
-	ret = dax_direct_access(cxl_dax_device, 0, nr_of_fat_pages, DAX_ACCESS, &alloc_table_start, &begin_pfn);
+	ret = dax_direct_access(cxl_dax_device, 0, FAT_OFFSET, DAX_ACCESS, &alloc_table_start, &begin_pfn);
 	if (ret < 0) return ret;
 	volatile struct ownership *owner_on_mem = (volatile struct ownership *)alloc_table_start;
 	if (owner_on_mem) {
@@ -186,13 +186,12 @@ static int get_cxl_device(void) {
 		if (cxl_dax_device) {
 			pr_info("got dax_device\n");
 			dax_write_cache(cxl_dax_device, false);
-			long nr_of_fat_pages = FAT_OFFSET;
 			if (!dax_alive(cxl_dax_device))
 				run_dax(cxl_dax_device);
-			int ret = dax_direct_access(cxl_dax_device, 0, nr_of_fat_pages, DAX_ACCESS, &alloc_table_start, &begin_pfn);
+			int ret = dax_direct_access(cxl_dax_device, 0, FAT_OFFSET, DAX_ACCESS, &alloc_table_start, &begin_pfn);
 			if (ret < 0) return ret;
 			end_pfn = begin_pfn;
-			end_pfn.val = end_pfn.val + nr_of_fat_pages - 1;
+			end_pfn.val = end_pfn.val + FAT_OFFSET - 1;
 			pr_info("Initialise allocation table at 0x%llx to 0x%llx \n", begin_pfn.val, end_pfn.val);
 			is_owner(0);
 		} else {
