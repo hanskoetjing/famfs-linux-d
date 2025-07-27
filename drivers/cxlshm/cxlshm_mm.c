3e76ff30fa2b39acc389da3d722daad75bc72ae6
hanskoetjing <hanskucing@gmail.com>
2025-07-27 20:38:38 +0100

    change sleep duration

--- drivers/cxlshm/cxlshm_mm.c
+++ drivers/cxlshm/cxlshm_mm.c
@@ -98,7 +98,7 @@ static vm_fault_t cxl_helper_filemap_fault(struct vm_fault *vmf)
 			tcp_server_stop();
 		} else {
 			pr_info("waiting response\n");
-			fsleep(1000);
+			msleep(1000);
 		}
 	}
 	o.owner_pid = task->pid;
