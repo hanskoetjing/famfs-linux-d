28a160b42fc8b98bd40e50f5026021eb964ebf77
hanskoetjing <hanskucing@gmail.com>
2025-07-29 22:10:59 +0100

    missing a semicolon

--- drivers/cxlshm/conn_manager.c
+++ drivers/cxlshm/conn_manager.c
@@ -30,7 +30,7 @@ int tcp_server_start(void);
 void tcp_server_stop(void);
 void set_port(int port_param);
 void set_ownership_completion(struct completion *param);
-int send_response(char *response_message)
+int send_response(char *response_message);
 
 void set_port(int port_param) {
     open_port = port_param;
