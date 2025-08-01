4472b514a13883f7f822f40ff7ab329ba58f184e
hanskoetjing <hanskucing@gmail.com>
2025-08-01 02:55:45 +0100

    add prototype for set and reset ownership on mem

--- drivers/cxlshm/cxlshm-private.h
+++ drivers/cxlshm/cxlshm-private.h
@@ -20,6 +20,8 @@ int get_cxl_device(void);
 vm_fault_t handle_fault_on_cxldax(struct vm_fault *vmf);
 pid_t get_owner_pid_on_mem(void);
 void get_dest_host(char **dest_ip_4_address, int *dest_port);
+int reset_ownership(void);
+int set_ownership(pid_t pid, char *connection_string, unsigned long vma_start, unsigned long vma_end);
 
 //cxlshm_ownership_helper
 int change_ownership(pid_t current_owner, pid_t requestor);
