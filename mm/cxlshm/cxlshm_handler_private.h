da3bb03c47b0c2dc0531a3d7b50707e589b87ee1
hanskoetjing <hanskucing@gmail.com>
2025-08-05 00:46:11 +0100

    add syscall to set host id

--- mm/cxlshm/cxlshm_handler_private.h
+++ mm/cxlshm/cxlshm_handler_private.h
@@ -30,7 +30,7 @@ int get_cxl_dax_dev(char *device_path_param);
 int alloc_mem_on_devdax(char *device_path_param, unsigned long len, void **dax_kaddr, pfn_t *dax_pfn);
 int get_cxl_dax_device(char *device_path_param);
 vm_fault_t handle_fault_on_cxldaxdev(struct vm_fault *vmf);
-int is_allottable(pid_t requestor_pid, char *address, int port);
+int is_allottable(pid_t requestor_pid);
 int ask_for_permission(pid_t existing_owner, pid_t requestor_pid, char *address, int port);
 void get_current_device_path(char **dev_path);
 int get_owner_info_on_mem(struct ownership **owner);
--- mm/cxlshm/cxlshm_ownership_handler.c
+++ mm/cxlshm/cxlshm_ownership_handler.c
@@ -25,14 +25,43 @@ char this_host[24] = {0};
 
 int change_ownership(pid_t current_owner, pid_t requestor);
 void set_host(char *host_id_string);
-int is_allottable(pid_t requestor_pid, char *address, int port);
+int is_allottable(pid_t requestor_pid);
 int ask_for_permission(pid_t existing_owner, pid_t requestor_pid, char *address, int port);
 void set_new_ownership(struct ownership **new_owner, char *address, int port);
 
-int is_allottable(pid_t requestor_pid, char *address, int port) {
+int is_allottable(pid_t requestor_pid) {
 	struct ownership *owner;
 	pr_info(THIS_MOD "is_allottable function here %d\n", requestor_pid);
 	get_owner_info_on_mem(&owner);
+	char address[16] = {0};
+	int port = 0;
+	/*set host identifier on ownership data*/
+	if (strlen(this_host) == 0)
+	{
+		strscpy(address, "127.0.0.1", 16);
+		port = 57580;
+	}
+	else
+	{
+		char *ip_4_addr_from_user = strsep(&this_host, ":");
+		if (ip_4_addr_from_user != NULL) 
+		{
+			int port_from_user = 0;
+			int strtoint_ret = kstrtoint(this_host, 10, &port_from_user);
+			if (strtoint_ret >= 0) 
+			{
+				strscpy(address, ip_4_addr_from_user, sizeof(address));
+				port = port_from_user;
+			}
+		}
+		else
+		{
+			strscpy(address, "127.0.0.1", 16);
+			port = 57580;
+		}
+	}
+
+	/*ownership checking here, also ask for permission if needed*/
 	if (owner->owner_pid <= 0)
 	{
 		pr_info(THIS_MOD "nobody owns this area\n");
@@ -140,57 +169,13 @@ void set_host(char *host_id_string) {
 }
 EXPORT_SYMBOL(set_host);
 
-/*
-int change_ownership(pid_t current_owner, pid_t requestor) {
-    int ret = 0;
-    pr_info(THIS_MOD "current owner pid: %d, requestor pid: %d\n", current_owner, requestor);
-    if (current_owner == requestor) {
-        pr_info(THIS_MOD "same pid. Do nothing and continue\n");
-        ret = 1;
-    } else if (current_owner < 0) {
-		pr_info(THIS_MOD "no ownership info. Taking over\n");
-		set_ownership(requestor, this_host, 0, 0);
-        ret = 1;
-	} else {
-        pr_info(THIS_MOD "different pid. takeover ownership\n");
-        char pid_to_send[16] = {0};
-		snprintf(pid_to_send, 15, "PID:%d", get_owner_pid_on_mem());
-		//snprintf(pid_to_send, 15, "PID:%d", requestor);
-        pr_info(THIS_MOD "message: %s\n", pid_to_send);
-        int dest_port = 0;
-        char *dest_ip_4_addr = kzalloc(sizeof(char) * 16, GFP_KERNEL);
-        memset(dest_ip_4_addr, 0, sizeof(char) * 16);
-        get_dest_host(&dest_ip_4_addr, &dest_port);
-        pr_info(THIS_MOD "%s %d\n", dest_ip_4_addr, dest_port);
-		tcp_client_start_d(dest_ip_4_addr, dest_port);
-		send_message_d(pid_to_send);
-		char received_copy[MAX_BUFFER_NET] = {0};
-		unsigned long timeout = msecs_to_jiffies(MAX_TIMEOUT_MSEC);
-		long completion_ret_val = wait_for_completion_interruptible_timeout(&is_complete, timeout);
-		if (completion_ret_val > 0) {
-			spin_lock(&client_lock);
-			strscpy(received_copy, response_received, sizeof(response_received));
-			memset(response_received, 0, sizeof(response_received));
-			spin_unlock(&client_lock);
-			if (strncmp(received_copy, "DONE", 4) == 0) {
-				set_ownership(requestor, this_host, 0, 0);
-				ret = 1;
-			} else {
-				pr_info(THIS_MOD "not a completion message, maybe handled later\n");
-				return -EINVAL;
-			}
-		} else if (completion_ret_val == 0) {
-			pr_info(THIS_MOD "timeout occured\n");
-			//automatically take over ownership if timeout occurred
-			set_ownership(requestor, this_host, 0, 0);
-			ret = 1;
-		} else {
-			pr_info(THIS_MOD "interrupted\n");
-			ret = -EAGAIN;
-		}
-		tcp_client_stop_d();
-    }
-    return ret;
+/*syscall to set host id. format: IP_V4:PORT*/
+SYSCALL_DEFINE1(set_process_identification, char __user *, process_id)
+{
+	char message_buf[24] = {0};
+	int ret = strncpy_from_user(message_buf, process_id, sizeof(message_buf));
+	if (ret < 0) return -EFAULT;
+	if (ret >= sizeof(message_buf) || ret == 0) return -EINVAL;
+	set_host(message_buf);
+	return 0;
 }
-EXPORT_SYMBOL(change_ownership);
-*/
