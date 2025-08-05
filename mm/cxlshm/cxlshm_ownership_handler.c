e914f2222fa2da836b899590a7b3feba0bfb9d8a
hanskoetjing <hanskucing@gmail.com>
2025-08-05 01:16:09 +0100

    add log

--- mm/cxlshm/cxlshm_ownership_handler.c
+++ mm/cxlshm/cxlshm_ownership_handler.c
@@ -54,7 +54,7 @@ int is_allottable(pid_t requestor_pid) {
 			int strtoint_ret = kstrtoint(temp_hostid_processing, 10, &port_from_user);
 			if (strtoint_ret >= 0) 
 			{
-				strscpy(address, ip_4_addr_from_user, sizeof(address) + 1);
+				strscpy(address, ip_4_addr_from_user, sizeof(address));
 				port = port_from_user;
 			}
 		}
@@ -64,7 +64,7 @@ int is_allottable(pid_t requestor_pid) {
 			port = 57580;
 		}
 	}
-
+	pr_info(THIS_MOD "host id: %s %p\n", address, port);
 	/*ownership checking here, also ask for permission if needed*/
 	if (owner->owner_pid <= 0)
 	{
