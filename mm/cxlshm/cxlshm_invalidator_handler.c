687a89a22fe4563bd75d5d5e6b059a4a13780556
hanskoetjing <hanskucing@gmail.com>
2025-08-09 23:25:46 +0100

    end is invalid

--- mm/cxlshm/cxlshm_invalidator_handler.c
+++ mm/cxlshm/cxlshm_invalidator_handler.c
@@ -132,7 +132,7 @@ int flush_mem_task(pid_t pid)
                 struct mm_struct *mm = this_vma->vm_mm;
                 struct mmu_gather tlb;
                 tlb_gather_mmu(&tlb, mm);
-                change_vma_protection_range(&tlb, this_vma, this_vma->vm_start, (this_vma->vm_start + owner_on_mem->offset), PAGE_NONE, MM_CP_UFFD_WP);
+                change_vma_protection_range(&tlb, this_vma, this_vma->vm_start, this_vma->vm_end, PAGE_NONE, MM_CP_UFFD_WP);
                 tlb_finish_mmu(&tlb);
                 flush_tlb_mm(mm);
                 flush_cache_range(this_vma, this_vma->vm_start, this_vma->vm_end);
