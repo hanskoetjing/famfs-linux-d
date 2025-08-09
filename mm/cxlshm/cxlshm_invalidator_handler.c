364f6ec716893dde073c6b142f66f5fd147cb135
hanskoetjing <hanskucing@gmail.com>
2025-08-10 00:45:25 +0100

    try to move invalidate

--- mm/cxlshm/cxlshm_invalidator_handler.c
+++ mm/cxlshm/cxlshm_invalidator_handler.c
@@ -122,8 +122,9 @@ int flush_mem_task(pid_t pid)
                     this_vma = vma;
                     struct mm_struct *mm = vma->vm_mm;
                     struct mmu_gather tlb;
-                    mmap_write_lock(mm);
                     invalidate_vma(vma);
+                    mmap_write_lock(mm);
+                    //invalidate_vma(vma);
                     zap_vma_ptes(vma, vma->vm_start, vma->vm_end - vma->vm_start); //temporar
                     flush_cache_range(vma, vma->vm_start, vma->vm_end);
                     flush_tlb_mm(mm);
