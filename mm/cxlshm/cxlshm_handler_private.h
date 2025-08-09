e010408ec36f7bb1301abf21f9e78a35da7b7d21
hanskoetjing <hanskucing@gmail.com>
2025-08-09 01:09:08 +0100

    .

--- mm/cxlshm/cxlshm_handler_private.h
+++ mm/cxlshm/cxlshm_handler_private.h
@@ -36,7 +36,7 @@ int get_cxl_dax_device(char *device_path_param);
 void get_current_device_path(char **dev_path);
 int get_owner_info_on_mem(struct ownership **owner);
 int set_owner_info_on_mem(struct ownership *owner);
-void set_new_owner_info(struct ownership **owner, pid_t pid, unsigned long vm_start, unsigned long vm_end, unsigned long offset);
+int set_new_owner_info(struct ownership **owner, pid_t pid, unsigned long vm_start, unsigned long vm_end, unsigned long offset);
 pfn_t get_pfn_by_offset(u64 offset);
 vm_fault_t handle_fault_on_cxldaxdev_prot(struct vm_fault *vmf, pgprot_t pgprot);
 vm_fault_t handle_fault_on_cxldaxdev_mkwrite(struct vm_fault *vmf);
